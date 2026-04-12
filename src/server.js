5bdc5633e224ae9285a80096c1d756040ba6778b
Paras Rathore <153386910+Pjaxparas@users.noreply.github.com>
2026-04-12 09:18:12 +0000

    set file path of sitemap.xml

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -31,3 +32,12 @@ app.get('/robots.txt', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`SYSTEM LIVE ON PORT ${PORT}`));
 
+// Sitemap route
+app.get('/sitemap.xml', (req, res) => {
+  res.sendFile(path.join(__dirname, 'sitemap.xml'));
+});
+
+// Robots.txt route
+app.get('/robots.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, 'robots.txt'));
+});
